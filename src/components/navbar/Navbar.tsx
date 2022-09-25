77419ad7e474cb2a0420f7c5cec467b5446490c7
yasin.gedik <yasin.gedik@akinon.com>
2022-09-25 16:03:50 +0300

    added webpact, declare file and tailwind theme config.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,10 @@
-export const Navbar = () => {
+import React from 'react'
+import styles from './Navbar.module.css';
+
+const Navbar: React.FC <{}> = () => {
   return (
     <div>Navbar</div>
   )
 }
+
+export default Navbar
